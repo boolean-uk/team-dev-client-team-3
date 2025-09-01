1f32556cccf3f46784c160d3b0ac6e773057eea8
Jonatan Berg Romundgard <jonatan.romundgard@no.experis.com>
2025-09-01 17:53:44 +0200

    Local email and password validation now works.

--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -8,10 +8,69 @@ import './register.css';
 const Register = () => {
   const { onRegister } = useAuth();
   const [formData, setFormData] = useState({ email: '', password: '' });
+  const [isEmailValid, setIsEmailValid] = useState(true);
 
   const onChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
+
+    if (name === 'email') {
+      const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9-]+(\.[A-Za-z0-9-]+)*\.[A-Za-z]{2,}$/;
+
+      const valid = emailRegex.test(value);
+      setIsEmailValid(valid);
+    }
+  };
+
+  const validateEmail = () => {
+    return '';
+  };
+
+  const validatePassword = () => {
+    if (valEightChars() && valNumber() && ValCapLetter() && ValSpecialChar()) {
+      // Replace with endpoint from backend for security.
+      console.log('Valid password');
+    } else {
+      console.log('Invalid password');
+    }
+  };
+
+  const valEightChars = () => {
+    if (formData.password.length >= 8) {
+      return true;
+    }
+    return false;
+  };
+
+  const ValCapLetter = () => {
+    const uppercaseRegex = /[A-Z]/;
+    if (uppercaseRegex.test(formData.password)) {
+      return true;
+    }
+    return false;
+  };
+
+  const valNumber = () => {
+    const numberRegex = /\d/;
+    if (numberRegex.test(formData.password)) {
+      return true;
+    }
+    return false;
+  };
+
+  const ValSpecialChar = () => {
+    const specialCharRegex = /[!@#$%^&*()\-=+{};:',./\\|~]/;
+    if (specialCharRegex.test(formData.password)) {
+      return true;
+    }
+    return false;
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (validateEmail() && validatePassword()) {
+      onRegister(formData.email, formData.password);
+    }
   };
 
   return (
@@ -31,6 +90,8 @@ const Register = () => {
               type="email"
               name="email"
               label={'Email *'}
+              className={!isEmailValid ? 'emailinvalid' : ''}
+              required
             />
             <TextInput
               value={formData.password}
@@ -39,15 +100,25 @@ const Register = () => {
               label={'Password *'}
               type={'password'}
             />
-            <p className="password-hint">
+            {/* <p className="password-hint">
               Password must contain at least eight characters, including at least one capital
               letter, one number and one special character
+            </p> */}
+            <p className="password-hint">
+              Password must contain at least: <br />
+              <ul className="password-hint-3">
+                <li className={valEightChars() ? 'valid' : 'invalid'}>- eight characters</li>
+                <li className={ValCapLetter() ? 'valid' : 'invalid'}>- one capital letter</li>
+                <li className={valNumber() ? 'valid' : 'invalid'}>- one number</li>
+                <li className={ValSpecialChar() ? 'valid' : 'invalid'}>- one special character</li>
+              </ul>
             </p>
             <p className="password-hint-2">*Required</p>
           </form>
           <Button
             text="Sign up"
-            onClick={() => onRegister(formData.email, formData.password)}
+            // onClick={() => onRegister(formData.email, formData.password)}
+            onClick={() => console.log(validatePassword())}
             classes="green width-full"
           />
         </div>
