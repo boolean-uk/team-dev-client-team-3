2c953a7ee4d60dec1b3f66e1be6545cab60bec7c
Chrissivert <122440126+Chrissivert@users.noreply.github.com>
2025-09-04 14:50:54 +0200

    removed more stuff

--- src/components/createPostModal/index.js
+++ src/components/createPostModal/index.js
@@ -1,28 +1,14 @@
 import { useState } from 'react';
-import useModal from '../../hooks/useModal';
 import './style.css';
 import Button from '../button';
-import Card from '../card'; // the post card
 
 const CreatePostModal = ({ onPostSubmit }) => {
-  const { closeModal } = useModal();
   const [text, setText] = useState('');
-  const [message, setMessage] = useState(null);
-
   const onChange = (e) => setText(e.target.value);
 
   const onSubmit = () => {
     if (!text.length) return;
-
-    // Call the callback passed from Dashboard
     onPostSubmit(text);
-
-    setMessage('Post created! Closing modal in 2 seconds...');
-    setTimeout(() => {
-      setMessage(null);
-      closeModal();
-      setText('');
-    }, 2000);
   };
 
   return (
@@ -52,8 +38,6 @@ const CreatePostModal = ({ onPostSubmit }) => {
           disabled={!text.length}
         />
       </section>
-
-      {message && <p>{message}</p>}
     </>
   );
 };
