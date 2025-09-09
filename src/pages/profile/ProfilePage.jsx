310b0333e00f04c258e167af540dbc7eb92c1caa
Chrissivert <122440126+Chrissivert@users.noreply.github.com>
2025-09-09 13:01:02 +0200

    some attributes of user is shown in profile

--- src/pages/profile/ProfilePage.jsx
+++ src/pages/profile/ProfilePage.jsx
@@ -1,23 +1,33 @@
+import React, { useState } from 'react';
 import './style.css';
 import TextInput from '../../components/form/textInput';
 import Form from '../../components/form';
 import ProfileCircle from '../../components/profileCircle';
 
 const ProfilePage = () => {
-  const user = {
-    firstName: 'Alex',
-    lastName: 'Jameson',
-    username: 'AlexUsername',
-    githubUsername: 'AlexGithub',
-    role: 'student',
-    specialization: 'frontend',
-    cohort: 'cohort 3',
-    startDate: 'someDate',
-    endDate: 'someDate',
-    email: 'alexjameson@mail.com',
-    mobile: 12345678,
-    password: 'somePassword'
-  };
+  const storedUser = JSON.parse(localStorage.getItem('user')) || {};
+  const [user, setUser] = useState({
+    firstName: storedUser.firstName || '',
+    lastName: storedUser.lastName || '',
+    username: storedUser.username || '',
+    githubUsername: storedUser.githubUsername || '',
+    role: storedUser.role || 'student',
+    specialization: storedUser.specialization || 'frontend',
+    cohort: storedUser.cohort || 'cohort 3',
+    startDate: storedUser.startDate || 'someDate',
+    endDate: storedUser.endDate || 'someDate',
+    email: storedUser.email || '',
+    mobile: storedUser.mobile || '',
+    password: storedUser.password || '',
+    bio: storedUser.bio || ''
+  });
+
+  const handleChange = (field, value) => {
+  const updatedUser = { ...user, [field]: value };
+  setUser(updatedUser);
+  localStorage.setItem('user', JSON.stringify(updatedUser));
+};
+
 
   return (
     <>
@@ -34,8 +44,8 @@ const ProfilePage = () => {
                   showMenu={false}
                 />
               </div>
-              <TextInput label={'First Name'} name="firstName" value={user.firstName} />
-              <TextInput label={'Last Name'} name="lastName" value={user.lastName} />
+              <TextInput label={'First Name'} name="firstName" value={user.firstName} onChange={e => handleChange('firstName', e.target.value)} />
+              <TextInput label={'Last Name'} name="lastName" value={user.lastName} onChange={e => handleChange('lastName', e.target.value)} />
               <TextInput label={'Username'} name="username" value={user.username} />
               <TextInput label={'Github Username'} name="githubName" value={user.githubUsername} />
             </div>
@@ -75,12 +85,12 @@ const ProfilePage = () => {
               <textarea
                 className="bio"
                 maxLength={300}
-                disabled
                 id="bio"
                 name="bio"
                 value={user.bio}
+                onChange={e => handleChange('bio', e.target.value)}
               ></textarea>
-              <span id="charCount">0/300</span>
+              <span id="charCount">{user.bio ? user.bio.length : 0}/300</span>
             </div>
           </section>
         </Form>
