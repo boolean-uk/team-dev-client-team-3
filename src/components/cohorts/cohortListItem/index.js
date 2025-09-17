7c33bbbc022b769b81cdbd714ee264eab47cc464
Hans Jakob Håland <Hans.Jakob.Haland@no.experis.com>
2025-09-17 11:19:00 +0200

    Adds TODO comments for Cohorts component

--- src/components/cohorts/cohortListItem/index.js
+++ src/components/cohorts/cohortListItem/index.js
@@ -1,11 +1,14 @@
 const CohortListItem = ({ cohort }) => {
+  // TODO: Add icon
+  // TODO: Add betteer two-line display of text
+  // TODO: Add scroll bar if more than 4 items
   return (
     <div>
-    <div className="profile-circle">
-              <div className="profile-icon" style={{ background: "green" }}>
-                {<p></p>}
-              </div>
-    </div>
+      <div className="profile-circle">
+        <div className="profile-icon" style={{ background: 'green' }}>
+          {<p></p>}
+        </div>
+      </div>
 
       <h5>{cohort.name}</h5>
       <br />
