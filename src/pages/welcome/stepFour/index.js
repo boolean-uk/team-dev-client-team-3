5391e4a2a54b321082abdaad58a395c595d37286
Chrissivert <122440126+Chrissivert@users.noreply.github.com>
2025-09-19 11:39:09 +0200

    added characters restriction on profile signup

--- src/pages/welcome/stepFour/index.js
+++ src/pages/welcome/stepFour/index.js
@@ -1,20 +1,31 @@
 import Form from '../../../components/form';
+import useAuth from '../../../hooks/useAuth';
 
-const StepFour = ({ data, setData }) => {
+const StepFour = ({ data, setData, maxLength = 300 }) => {
+
   return (
     <>
       <div className="welcome-formheader">
         <h3>Bio</h3>
       </div>
-      <Form className="welcome-form">
-        <div className="welcome-form-inputs">
-          <textarea
-            name="bio"
-            value={data.bio}
-            onChange={setData}
-            placeholder="Write here"
-          ></textarea>
-        </div>
+      <Form>
+        <section>
+          <div className="welcome-form-inputs">
+            <div>
+              <textarea
+                maxLength={maxLength}
+                id="bio"
+                name="bio"
+                value={data.bio || ''}
+                onChange={setData}
+                placeholder="Write here"
+              />
+              <span id="charCount">
+                {data.bio?.length ?? 0}/{maxLength}
+              </span>
+            </div>
+          </div>
+        </section>
       </Form>
     </>
   );
