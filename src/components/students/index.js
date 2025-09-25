16e7199c6fcb086600fa966197b70e6ce9fe045c
Roman Eriksen <roman.eriksen@no.experis.com>
2025-09-25 10:51:23 +0200

    Fixed student and teacher listing

--- src/components/students/index.js
+++ src/components/students/index.js
@@ -2,11 +2,11 @@ import { Link } from 'react-router-dom';
 import { SingleLineListItem } from '../avatarList/singleLineListItem';
 import './style.css';
 
-const Students = ({ data = [] }) => (
+const Students = ({ data = [], listClassName = 'students-list' }) => (
   <div className="students-card">
     <h4>Students</h4>
     <hr className="avatar-list-divider" />
-    <div className="students-list">
+    <div className={listClassName}>
       {data.map((student, index) => (
         <Link to={`/profile/${student.id}`} key={`${student.id}-${index}`} className="student-link">
           <SingleLineListItem user={student} />
